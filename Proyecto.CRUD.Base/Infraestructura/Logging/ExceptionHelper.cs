ff37bc86ca375169fad211df4d711c69e8a48031
ivanchuelo14 <39717821+ivanchuelo14@users.noreply.github.com>
2024-02-01 02:08:47 -0500

    Ajuste de logger

---
+++ Proyecto.CRUD.Base/Infraestructura/Logging/ExceptionHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Proyecto.CRUD.Base.Infraestructura.Logging
+{
+    public static class ExceptionHelper
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static string GetCurrentMethod()
+        {
+            var st = new StackTrace();
+            var sf = st.GetFrame(1);
+
+            return sf.GetMethod().Name;
+        }
+    }
+}
